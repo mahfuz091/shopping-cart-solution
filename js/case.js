7cfd53a38bc032dcbcc7a752a91c684d7fd55891
mahfuz091 <engmahfuz14@gmail.com>
2023-02-14 21:29:03 +0600

    added case btn js

--- js/case.js
+++ js/case.js
@@ -1,6 +1,15 @@
-document.getElementById('btn-case').addEventListener('click', function(){
-    const caseValueString = document.getElementById('case-value').value;
-    const caseValue = parseFloat(caseValueString);
-    console.log(caseValue);
-
+document.getElementById('btn-case-plus').addEventListener('click', function(){
+    const caseValueField = document.getElementById('case-input');
+    const caseValueString = caseValueField.value;
+    const previousCaseValue = parseFloat(caseValueString);
+    const newCaseValue = previousCaseValue + 1;
+    caseValueField.value = newCaseValue 
+   
+})
+document.getElementById('btn-case-minus').addEventListener('click', function(){
+    const caseValueField = document.getElementById('case-input');
+    const caseValueString = caseValueField.value;
+    const previousCaseValue = parseFloat(caseValueString);
+    const newCaseValue = previousCaseValue - 1;
+    caseValueField.value = newCaseValue
 })
